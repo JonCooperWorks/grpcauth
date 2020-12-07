d43eaaf1fdf3505f7033a1a362d38e241c49086c
Jonathan Cooper <cooper@cooperthecoder.com>
2020-12-07 13:48:42 -0500

    fixed typo

--- authority.go
+++ authority.go
@@ -47,7 +47,7 @@ type AuthFunc func(md metadata.MD) (*AuthResult, error)
 // PermissionFunc determines if an authenticated client is authorized to access a particular gRPC method.
 // It allows users to override the default permission behaviour that requires a permission with the full gRPC
 // method name be sent over during authentication.
-type PermissionFunc func(permissions []string, methodnName string) bool
+type PermissionFunc func(permissions []string, methodName string) bool
 
 // NoPermissions permits a gRPC client unlimited access to all methods on the server.
 // It allows for servers that grant authenticated clients access to all methods on a gRPC server.
